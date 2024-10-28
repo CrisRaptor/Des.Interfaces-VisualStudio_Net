5f1f996703c40e0cff1dd50f55d545e3ef12ad99
CrisR <cristiangarcha7@gmail.com>
2024-10-28 13:57:06 +0100

    TextFieldComponent
    Nuevo component custom, textField, falta el atributo para elegir si alinear el nombre con el segundo label o el editText

---
+++ textField/textField/textField.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace textField
+{
+    partial class textField
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFieldName = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelLimit = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelFieldName
+            // 
+            this.labelFieldName.AutoSize = true;
+            this.labelFieldName.Location = new System.Drawing.Point(0, 0);
+            this.labelFieldName.Name = "labelFieldName";
+            this.labelFieldName.Size = new System.Drawing.Size(44, 16);
+            this.labelFieldName.TabIndex = 0;
+            this.labelFieldName.Text = "Name";
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(0, 19);
+            this.textBox.MaxLength = 50;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(150, 22);
+            this.textBox.TabIndex = 1;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // labelLimit
+            // 
+            this.labelLimit.AutoSize = true;
+            this.labelLimit.Location = new System.Drawing.Point(109, 0);
+            this.labelLimit.Name = "labelLimit";
+            this.labelLimit.Size = new System.Drawing.Size(38, 16);
+            this.labelLimit.TabIndex = 2;
+            this.labelLimit.Text = "0 / 50";
+            // 
+            // textField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.labelLimit);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.labelFieldName);
+            this.Name = "textField";
+            this.Size = new System.Drawing.Size(150, 45);
+            this.Load += new System.EventHandler(this.textField_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFieldName;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label labelLimit;
+    }
+}
---
+++ textField/textField/textField.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textField
+{
+    public partial class textField: UserControl
+    {
+        public int CharacterLimit
+        {
+            get
+            {
+                return textBox.MaxLength;
+            }
+            set
+            {
+                textBox.MaxLength = value;
+                setLabelLimit();
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return labelFieldName.Text;
+            }
+            set
+            {
+                labelFieldName.Text = value;
+            }
+        }
+        public textField()
+        {
+            InitializeComponent();
+        }
+
+        private void textField_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setLabelLimit()
+        {
+            labelLimit.Text = textBox.Text.Length + " / " + textBox.MaxLength;
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            setLabelLimit();
+        }
+    }
+}
