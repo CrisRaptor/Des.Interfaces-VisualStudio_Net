48da2049a387973d3253a5f93fb581031fd3a2d0
CrisR <cristiangarcha7@gmail.com>
2024-11-18 10:40:23 +0100

    Conectar a base de datos postgres

--- Informes/Informes/Form1.Designer.cs
+++ Informes/Informes/Form1.Designer.cs
@@ -29,26 +29,38 @@
         private void InitializeComponent()
         {
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "Informes.Informe.Report1.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Location = new System.Drawing.Point(0, 344);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(4);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.Size = new System.Drawing.Size(1070, 210);
             this.reportViewer1.TabIndex = 0;
+            // 
+            // reportViewer2
+            // 
+            this.reportViewer2.LocalReport.ReportEmbeddedResource = "Informes.Informe.Report2.rdlc";
+            this.reportViewer2.Location = new System.Drawing.Point(0, -2);
+            this.reportViewer2.Name = "reportViewer2";
+            this.reportViewer2.ServerReport.BearerToken = null;
+            this.reportViewer2.Size = new System.Drawing.Size(1070, 339);
+            this.reportViewer2.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.reportViewer2);
             this.Controls.Add(this.reportViewer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "z";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -57,6 +69,7 @@
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
     }
 }
 
--- Informes/Informes/Form1.cs
+++ Informes/Informes/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Reporting.WinForms;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,8 +21,11 @@ namespace Informes
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: esta línea de código carga datos en la tabla 'empleadosDataSet.departamentos' Puede moverla o quitarla según sea necesario.
             rellenarDataSet();
+            connectToDatabase("empleados", "SELECT * FROM departamentos");
             this.reportViewer1.RefreshReport();
+            this.reportViewer2.RefreshReport();
         }
 
         private void rellenarDataSet()
@@ -35,6 +39,25 @@ namespace Informes
             ReportDataSource reportDataSource = new ReportDataSource("DataSet1", notas);
             reportViewer1.LocalReport.DataSources.Clear();
             reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+        }
+
+        private void connectToDatabase(string database, string sql)
+        {
+            string conection = "Server=localhost; Port=5432; User Id=postgres; Password=iesbelen; Database=" + database;
+            using (NpgsqlConnection conn = new NpgsqlConnection(conection))
+            {
+                conn.Open();
+
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                ReportDataSource reportDataSource = new ReportDataSource("datosEmpleados",dt);
+                reportViewer2.LocalReport.DataSources.Clear();
+                reportViewer2.LocalReport.DataSources.Add(reportDataSource);
+            }
+           
         }
     }
 }
